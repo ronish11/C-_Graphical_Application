138ea163a54d20e4a18bf786f68fda331614c215
yagya raj sharma <oneshivathreebaba@outlook.com>
2020-01-03 21:01:03 +0545

    component 2 task 2

--- Csharp_graphical_application/Polygon.cs
+++ Csharp_graphical_application/Polygon.cs
@@ -15,7 +15,7 @@ namespace Csharp_graphical_application
             try
             {
                 Pen p = new Pen(Color.Black);
-                Point[] a = { new Point(x, y), new Point(x * 2, y * 2), new Point(x * 3, x * 3), new Point(x * 2, y * 1) };
+                Point[] a = { new Point(x, y), new Point(x * 2, y * 2), new Point(x * 3, x * 3), new Point(x * 2, y * 1), new Point(x, y), new Point(x * 2, y * 2), new Point(x * 3, x * 3), new Point(x * 2, y * 1), new Point(x, y), new Point(x * 2, y * 2), new Point(x * 3, x * 3), new Point(x * 2, y * 1) };
                 g.DrawPolygon(p, a);
             }
             catch (Exception ex)
@@ -24,8 +24,25 @@ namespace Csharp_graphical_application
                 throw ex;
             }
         }
-
+        //Polygon 3,4,5,6,6
         public void SetParam(int x, int y, int width, int height)
+        {
+            try
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        public void set(params int[] list)
         {
             try
             {
